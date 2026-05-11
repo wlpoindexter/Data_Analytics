e420b82d548c04b3ff3818788e84bb45abe5921b
Will Poindexter <wlpoindexter16@gmail.com>
2026-05-10 21:12:44 -0500

    completed excercise 1.a

--- week_5/Ex1A_hello.py
+++ week_5/Ex1A_hello.py
@@ -1,1 +1,18 @@
 print("Hello world!")
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world" prints twice because there are two separate print statements: line 1 prints the string directly, line 4 prints the variable 'message' which holds the same string.
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+cents = cents + .25
+print(dollars + cents)
+# The output of the above code will be 3.75, which is the sum of the dollars and cents.
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
